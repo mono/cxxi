9627cc2fbdaa8710679de9160387b5917d4b657a
Steve <amn3sia@gmail.com>
2012-02-21 17:06:48 -0500

    Added more tests for testing primitive return types.
    Signed-off-by: Steve <amn3sia@gmail.com>

--- tests/MarshalingTests.cs
+++ tests/MarshalingTests.cs
@@ -23,6 +23,52 @@ namespace Tests {
 			Assert.AreEqual (44, c3.GetX (), "#3");
 		}
 
+        [Test]
+        public void TestPrimitiveReturn()
+        {
+            var sc = Class.ReturnSChar(68);
+            Assert.AreEqual((char)68, sc, "#1");
+
+            var c = Class.ReturnChar(69);
+            Assert.AreEqual((char)69, c, "#2");
+
+            var uc = Class.ReturnUChar(70);
+            Assert.AreEqual((char)70, uc, "#3");
+
+            var s = Class.ReturnShort(71);
+            Assert.AreEqual((short)71, s, "#4");
+
+            var us = Class.ReturnUShort(72);
+            Assert.AreEqual((ushort)72, us, "#5");
+
+            var i = Class.ReturnInt(73);
+            Assert.AreEqual((int)73, i, "#6");
+
+            var ui = Class.ReturnUInt(74);
+            Assert.AreEqual((uint)74, ui, "#7");
+
+            var l = Class.ReturnLong(75);
+            Assert.AreEqual((int)75, l, "#8");
+
+            var ul = Class.ReturnULong(76);
+            Assert.AreEqual((uint)76, ul, "#9");
+
+            var f = Class.ReturnFloat(77);
+            Assert.AreEqual((float)77, f, "#10");
+
+            var d = Class.ReturnDouble(78);
+            Assert.AreEqual((double)78, d, "#11");
+
+            var ld = Class.ReturnDouble(79);
+            Assert.AreEqual((double)79, ld, "#12");
+
+            var bt = Class.ReturnBool(80);
+            Assert.AreEqual(true, bt, "#13");
+
+            var bf = Class.ReturnBool(0);
+            Assert.AreEqual(false, bf, "#14");
+        }
+
 		// An object as ref argument
 		[Test]
 		public void TestClassArg ()
--- tests/Native/MarshalingTests.h
+++ tests/Native/MarshalingTests.h
@@ -78,6 +78,20 @@ public:
 	int& GetXRef () {
 		return x;
 	}
+	
+	static int ReturnInt(int x) { return x; }
+	static unsigned int ReturnUInt(int x) { return (unsigned int)x; }
+	static float ReturnFloat(int x) { return (float)x; }
+	static double ReturnDouble(int x) { return (double)x; }
+	static long double ReturnLDouble(int x) { return (long double)x; }
+	static bool ReturnBool(int x) { return (x != 0); }
+	static short ReturnShort(int x) { return (short)x; }
+	static unsigned short ReturnUShort(int x) { return (unsigned short)x; }
+	static long ReturnLong(int x) { return (long)x; }
+	static unsigned long ReturnULong(int x) { return (unsigned long)x; }
+	static char ReturnChar(int x) { return (char)x; }
+	static unsigned char ReturnUChar(int x) { return (unsigned char)x; }
+	static signed char ReturnSChar(int x) { return (signed char)x; }
 };
 
 		
