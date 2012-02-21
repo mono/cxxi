252e2a33b2e9c48f50e115ecd92f4b766511dcb9
Steve <amn3sia@gmail.com>
2012-02-21 16:37:58 -0500

    Added several more name mangling tests to really test the name mangling and compression.
    Signed-off-by: Steve <amn3sia@gmail.com>

--- tests/ManglingTests.cs
+++ tests/ManglingTests.cs
@@ -9,14 +9,32 @@ namespace Tests {
 		[Test]
 		public void TestCompression ()
 		{
+            float f1 = 0.0f, f2 = 0.0f;
+            int i1 = 0, i2 = 0;
+            double d1 = 0.0d, d2 = 0.0d;
 			Compression.Test1 (null, "foo", null, "bar");
+
+            var r1 = Compression.Test2(null, ref f1, ref i1, null, ref f2, ref i2, ref d1, ref d2, 5);
+            Assert.AreEqual(5, r1, "#2");
 		}
 
 		[Test]
 		public void TestNamespaced ()
 		{
+            Compression c = null;
+            Tests.Ns1.Namespaced n = null;
+            float f1 = 0.0f, f2 = 0.0f;
 			Ns1.Namespaced.Test1 ();
 			Ns1.Namespaced.Test2 (null);
+
+            var r3 = Ns1.Namespaced.Test3(c, c, ref f1, ref f2, 22);
+            Assert.AreEqual(22, r3, "#3");
+
+            var r4 = Ns1.Namespaced.Test4(n, n, c, c, ref f1, ref f2, 44);
+            Assert.AreEqual(44, r4, "#4");
+
+            var r5 = Ns1.Namespaced.Test5(n, n, c, c, 88);
+            Assert.AreEqual(88, r5, "#5");
 		}
 
 		[Test]
@@ -25,6 +43,22 @@ namespace Tests {
 			var cls = new Ns1.Ns2.Namespaced2 ();
 			cls.Test1 ();
 			cls.Test2 (null);
+            
+            Compression c = null;
+            Tests.Ns1.Namespaced n = null;
+            Tests.Ns1.Ns2.Namespaced2 n2 = null;
+            float f1 = 0.0f, f2 = 0.0f;
+            var r3 = Ns1.Ns2.Namespaced2.Test3(n2, n2, n, n, 11);
+            Assert.AreEqual(11, r3, "#3");
+
+            var r4 = Ns1.Ns2.Namespaced2.Test4(n2, n2, n, n, n2, n2, n, n, 111);
+            Assert.AreEqual(111, r4, "#4");
+
+            var r5 = Ns1.Ns2.Namespaced2.Test5(n2, n2, c, c, 1111);
+            Assert.AreEqual(1111, r5, "#5");
+
+            var r6 = Ns1.Ns2.Namespaced2.Test6(n2, n2, ref f1, ref f2, 11111);
+            Assert.AreEqual(11111, r6, "#6");
 		}
 	}
 }
--- tests/Native/ManglingTests.h
+++ tests/Native/ManglingTests.h
@@ -9,6 +9,8 @@
 class EXPORT Compression {
 public:
 	static void Test1 (const Compression* a1, const char* a2, const Compression* a3, const char* a4);
+	static int Test2 (const char *c1, const float *f1, const int *i1,
+		const char *c2, const float *f2, const int *i2, double *d1, double *d2, int i) { return i; }
 };
 
 namespace Ns1 {
@@ -16,6 +18,9 @@ namespace Ns1 {
 	public:
 		static void Test1 ();
 		static void Test2 (const Compression* a1);
+		static int Test3 (const Compression *a1, const Compression *a2, float *f1, float *f2, int i) { return i; }
+		static int Test4 (const Namespaced *n1, const Namespaced *n2, const Compression* a1, const Compression* a2, float *f1, float *f2, int i) { return i; }
+		static int Test5 (Namespaced *n1, Namespaced *n2, const Compression* a1, const Compression* a2, int i) { return i; }
 	};
 }
 
@@ -25,5 +30,10 @@ namespace Ns1 { namespace Ns2 {
 		Namespaced2 ();
 		void Test1 ();
 		Namespaced2* Test2 (Compression* a1);
+		static int Test3 (Namespaced2 *n21, Namespaced2 *n22, Namespaced *n1, Namespaced *n2, int i) { return i; }
+		static int Test4 (const Namespaced2 *n21, Namespaced2 *n22, const Namespaced *n1, Namespaced *n2,
+			const Namespaced2 *n23, Namespaced2 *n24, const Namespaced *n3, Namespaced *n4, int i) { return i; }
+		static int Test5 (const Namespaced2 *n21, const Namespaced2 *n22, const Compression *a1, const Compression *a2, int i) { return i; }
+		static int Test6 (const Namespaced2 *n21, const Namespaced2 *n22, float *f1, float *f2, int i) { return i; }
 	};
 }}
