b5e82274d8ffd8a8229e6e79d29f8c49576e8179
Steve <amn3sia@gmail.com>
2012-03-03 00:20:39 -0500

    Modified the template tests and added them to the project now that we have working generator code.
    Signed-off-by: Steve <amn3sia@gmail.com>

--- tests/TemplateTests.cs
+++ tests/TemplateTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using NUnit.Framework;
 
@@ -12,41 +11,41 @@ namespace Tests
         [Test]
         public void TestDoubleTemplate()
         {
-            var a = new AdderDouble();
-            Assert.AreEqual(0.0d, t.Number, "#1");
+            var a = new AdderTemplateOfDouble();
+            Assert.AreEqual(0.0d, a.Number, "#1");
 
             a.Add(5.33d);
-            Assert.AreEqual(5.33d, t.Number, "#2");
+            Assert.AreEqual(5.33d, a.Number, "#2");
         }
 
         [Test]
-        public void TestDoubleTemplate()
+        public void TestFloatTemplate()
         {
-            var a = new AdderFloat(2.5f);
-            Assert.AreEqual(2.5f, t.Number, "#1");
+            var a = new AdderTemplateOfFloat(2.5f);
+            Assert.AreEqual(2.5f, a.Number, "#1");
 
             a.Add(2.5f);
-            Assert.AreEqual(5.0f, t.Number, "#2");
+            Assert.AreEqual(5.0f, a.Number, "#2");
         }
 
         [Test]
         public void TestIntTemplate()
         {
-            var a = new AdderInt(10);
-            Assert.AreEqual(10, t.Number, "#1");
+            var a = new AdderTemplateOfInt(10);
+            Assert.AreEqual(10, a.Number, "#1");
 
             a.Add(-5);
-            Assert.AreEqual(5, t.Number, "#2");
+            Assert.AreEqual(5, a.Number, "#2");
         }
 
         [Test]
         public void TestShortTemplate()
         {
-            var a = new AdderShort();
-            Assert.AreEqual(0, t.Number, "#1");
+            var a = new AdderTemplateOfShortInt();
+            Assert.AreEqual(0, a.Number, "#1");
 
             a.Add(123);
-            Assert.AreEqual(123, t.Number, "#2");
+            Assert.AreEqual(123, a.Number, "#2");
         }
     }
 }
