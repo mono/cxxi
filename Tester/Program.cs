18668b710f0bd6b526f69d3e3e08d6b3a918ecef
Lap Kiu Tang <INOVAGEO\ltang@birns.inovageo.com>
2012-07-03 16:10:37 +0100

    Modifed the test program so it uses all the test classes.

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -9,6 +9,35 @@ namespace Tester
     {
         static void Main (string[] args)
         {
+			var fieldTest = new FieldTests();
+			fieldTest.TestReadCppObject();
+
+			var inheritTest = new InheritanceTests();
+			inheritTest.TestManagedOverride1();
+			inheritTest.TestManagedOverride2();
+			inheritTest.TestMultipleBases();
+			inheritTest.TestMultipleVirtualBases();
+			inheritTest.TestNonVirtualCallOnVirtualBaseClass();
+			inheritTest.TestRoundtripManagedOverride();
+			inheritTest.TestVirtualCall();
+			inheritTest.TestVirtualCallOnBaseClass();
+			inheritTest.TestVirtualCallOnVirtualBaseClass();
+
+			var marshalingTest = new MarshalingTests();
+			marshalingTest.TestByRefReturn();
+			marshalingTest.TestClassArg();
+			marshalingTest.TestClassArgByval();
+			marshalingTest.TestClassArgByvalNull();
+			marshalingTest.TestClassArgNull();
+			marshalingTest.TestClassReturn();
+			marshalingTest.TestPrimitiveReturn();
+
+			var templateTest = new TemplateTests();
+			templateTest.TestDoubleTemplate();
+			templateTest.TestFloatTemplate();
+			templateTest.TestIntTemplate();
+			templateTest.TestShortTemplate();
+
             var tests = new ManglingTests();
             tests.TestCompression();
         }
